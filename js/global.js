6c445e6eeeb9c11f6fea0e63c8c94bedc4f58066
Benjamin Rubin <bennyfrubin@gmail.com>
2019-08-12 13:04:16 -0700

    Update global.js

--- js/global.js
+++ js/global.js
@@ -24,4 +24,15 @@ $(document).ready(function() {
         $homelinks.removeClass('home-link-faded');
     });
 
+
+    $sidebarWrapper = $("#sidebar-wrapper");
+    // Hide Nav when page is wider than 
+    // removing the navigation backdrop if screen size increases
+    $(window).resize(function() {
+        if ($(window).width() > 767 && $sidebarWrapper.hasClass("active")) {
+            $sidebarWrapper.removeClass("active");
+            $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+        }
+    });
+
 });
