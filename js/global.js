0ae07bfc3bb4a2ffa30776532358e0ac99a9e803
Benjamin Rubin <bennyfrubin@gmail.com>
2019-09-08 20:28:48 -0700

    A bunch of updates

--- js/global.js
+++ js/global.js
@@ -37,4 +37,24 @@ $(document).ready(function() {
         }
     });
 
+
+
+    // Scroll to top button appear
+    $(document).scroll(function() {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+    // Smooth scrolling using jQuery easing
+    $('.scroll-to-top').click(function() {
+        document.querySelector('#page-top').scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+
+
 });
