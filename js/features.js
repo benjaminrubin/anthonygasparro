89724f2b422d4b1f9787c32e88d3850a32af5f6f
Benjamin Rubin <bennyfrubin@gmail.com>
2020-01-14 20:25:54 -0800

    Update

--- js/features.js
+++ js/features.js
@@ -1,11 +1,19 @@
 // Array containing info on all features:
 
 var features = [{
-    title: "Certain Women",
-    subtitle: "dir. Kelly Reichardt",
-    thumbnailUrl: "thumb_a.jpg",
-    videoUrl: "https://player.vimeo.com/video/353812069?title=0&byline=0&portrait=0",
-    padding: "56.25% 0 0 0",
-}];
+        title: "Certain Women",
+        subtitle: "dir. Kelly Reichardt",
+        thumbnailUrl: "thumb_a.jpg",
+        videoUrl: "https://player.vimeo.com/video/353812069?title=0&byline=0&portrait=0",
+        padding: "56.25% 0 0 0",
+    },
+    {
+        title: "First Cow",
+        subtitle: "dir. Kelly Reichardt",
+        thumbnailUrl: "thumb_b.jpg",
+        videoUrl: "https://player.vimeo.com/video/384807364?title=0&byline=0&portrait=0",
+        padding: "56.25% 0 0 0",
+    }
+];
 
 var contentType = "features";
