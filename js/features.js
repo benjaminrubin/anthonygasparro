c1a57f1277d249e9dd4a0431a36aa822f9e201c4
Benjamin Floyd Rubin <bennyfrubin@gmail.com>
2020-01-27 15:26:57 -0800

    Updated posters

--- js/features.js
+++ js/features.js
@@ -3,14 +3,14 @@
 var features = [{
         title: "Certain Women",
         subtitle: "dir. Kelly Reichardt",
-        thumbnailUrl: "thumb_a.jpg",
+        thumbnailUrl: "certain_women.jpg",
         videoUrl: "https://player.vimeo.com/video/353812069?title=0&byline=0&portrait=0",
         padding: "56.25% 0 0 0",
     },
     {
         title: "First Cow",
         subtitle: "dir. Kelly Reichardt",
-        thumbnailUrl: "thumb_b.jpg",
+        thumbnailUrl: "first_cow.jpg",
         videoUrl: "https://player.vimeo.com/video/384807364?title=0&byline=0&portrait=0",
         padding: "56.25% 0 0 0",
     }
