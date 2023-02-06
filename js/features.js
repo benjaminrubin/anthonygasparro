5fab901487229a9a7f2381fdd39f65667de3cefb
Benjamin Floyd Rubin <bennyfrubin@gmail.com>
2023-02-06 14:01:11 -0800

    Updates

--- js/features.js
+++ js/features.js
@@ -3,17 +3,26 @@
 var features = [{
         title: "Certain Women",
         subtitle: "dir. Kelly Reichardt",
-        thumbnailUrl: "certain_women.jpg",
+        thumbnailUrl: "certain_women.png",
         videoUrl: "https://player.vimeo.com/video/353812069?title=0&byline=0&portrait=0",
         padding: "56.25% 0 0 0",
     },
     {
         title: "First Cow",
         subtitle: "dir. Kelly Reichardt",
-        thumbnailUrl: "first_cow.jpg",
+        thumbnailUrl: "first_cow.png",
         videoUrl: "https://player.vimeo.com/video/384807364?title=0&byline=0&portrait=0",
+        padding: "56.25% 0 0 0",
+    },
+    {
+        title: "Showing Up",
+        subtitle: "dir. Kelly Reichardt",
+        thumbnailUrl: "showing_up.png",
+        videoUrl: "https://player.vimeo.com/video/796427640?title=0&byline=0&portrait=0",
         padding: "56.25% 0 0 0",
     }
 ];
 
 var contentType = "features";
+
+
