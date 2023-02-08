4737a2554f3332a0bb3ce3efdaeb04b270847ff1
Benjamin Floyd Rubin <bennyfrubin@gmail.com>
2023-02-08 11:32:50 -0800

    reordered features page

--- js/features.js
+++ js/features.js
@@ -1,10 +1,11 @@
 // Array containing info on all features:
 
-var features = [{
-        title: "Certain Women",
+var features = [
+    {
+        title: "Showing Up",
         subtitle: "dir. Kelly Reichardt",
-        thumbnailUrl: "certain_women.png",
-        videoUrl: "https://player.vimeo.com/video/353812069?title=0&byline=0&portrait=0",
+        thumbnailUrl: "showing_up.png",
+        videoUrl: "https://player.vimeo.com/video/796427640?title=0&byline=0&portrait=0",
         padding: "56.25% 0 0 0",
     },
     {
@@ -15,12 +16,12 @@ var features = [{
         padding: "56.25% 0 0 0",
     },
     {
-        title: "Showing Up",
+        title: "Certain Women",
         subtitle: "dir. Kelly Reichardt",
-        thumbnailUrl: "showing_up.png",
-        videoUrl: "https://player.vimeo.com/video/796427640?title=0&byline=0&portrait=0",
+        thumbnailUrl: "certain_women.png",
+        videoUrl: "https://player.vimeo.com/video/353812069?title=0&byline=0&portrait=0",
         padding: "56.25% 0 0 0",
-    }
+    },
 ];
 
 var contentType = "features";
